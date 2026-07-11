0a2353e19822979046c8448ddf318238dc4dbfaa
Ash-exwo <aswathygr61@gmail.com>
2026-07-11 12:13:03 +0530

    Delete Registration Completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import StudRegistration from './components/StudRegistration'
 import SearchRegistration from './components/SearchRegistration'
+import DeleteRegistration from './components/DeleteRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path='/' element={<StudRegistration/>}/>
         <Route path='/search' element={<SearchRegistration/>}/>
+        <Route path='/delete' element={<DeleteRegistration/>}/>
       </Routes>
       </BrowserRouter>
     </>
