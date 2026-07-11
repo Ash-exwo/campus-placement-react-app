27060fa9d95087f27056d91a9afef5ba77835f01
Ash-exwo <aswathygr61@gmail.com>
2026-07-11 11:30:42 +0530

    Student Registration Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import StudRegistration from './components/StudRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<StudRegistration/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
